612600b94d9e2b793b68f2b6ac9c6c70dd852653
Paweł Placzyński <placzynski.pawel@gmail.com>
2017-05-04 10:45:22 +0200

    (suprisingly) revert adding "private"
    This reverts commit 2fcdc108dd9fa812f4c8ada5d01894d8689b174f.

--- lib/recurring_date_enumerator.rb
+++ lib/recurring_date_enumerator.rb
@@ -119,8 +119,6 @@ class RecurringDateEnumerator < Enumerator
     ['#<RecurringDateEnumerator: 0x', object_id, '>'].join
   end
 
-  private
-
   def self.infinity(date)
     Enumerator.new do |y|
       i = date.prev_day
